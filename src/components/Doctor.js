4b1444890f0ef557d53a0482583ab4b7b45c8049
Akash <57212963+Akash-152000@users.noreply.github.com>
2021-04-07 16:06:59 +0530

    Update Doctor.js

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -20,21 +20,25 @@ class Doctor extends Component{
 	render(){
 	  return(
 	    <React.Fragment>
-        {this.props.account=="0x89fae8e8a7e50Dc5abcebF9f6e26635061CefAC4"?
+        {this.props.account=="0x92517Be160b933E2aF95d66ac3637Acf173EeEec"?
            this.props.images.map((image, key) => {
                   return(
                     <div className="card mb-4" key={key} >
                       <ul id="imageList" className="list-group list-group-flush">
                         <li className="list-group-item">
-                          <p class="text-center"><img src={`https://ipfs.infura.io/ipfs/${image.hash}`} style={{ maxWidth: '420px'}}/></p>
-                          <p>{image.description}</p>
-                          <p>{image.date}</p>
+                        <p>{image.date}</p> 
+                          <p class="text-center"><iframe src={`https://ipfs.infura.io/ipfs/${image.hash}`} style={{ maxWidth: '420px'}}/></p>
+                          <p><b>{image.description}</b></p>  
+                          <p>Highest recorded :<b>{image.maxVal}</b></p> 
+                          <p>Lowest recorded :<b>{image.minVal}</b></p>
+                          
                         </li>    
                       </ul>
                     </div>
                   )
+
                 })
-          :<Dialog err={this.state.error}/>
+        :<Dialog err={this.state.error}/>
 
       }
 
